f5e699089764a28feb3a10eb900ed00075dcca4d
donghunee <ehdgns1766@naver.com>
2025-05-19 08:27:23 +0900

    update

--- src/components/auth/EmailVerification.tsx
+++ src/components/auth/EmailVerification.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, Input, Space, Form } from 'antd';
 import { useEmailVerification } from '../../hooks/api/auth/useEmailVerification';
 
